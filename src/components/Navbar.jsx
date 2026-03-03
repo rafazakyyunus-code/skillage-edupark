1a6cc98b7552d18e09a5e7d37bb92e392e951ae5
Lucky Ubaidillah <luckytrusted8@gmail.com>
2026-03-03 09:09:00 +0700

    navbar dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,38 @@ export default function Navbar() {
       </div>
 
       <ul className={menuOpen ? "nav-links active" : "nav-links"}>
-        <li>Beranda</li>
-        <li>Tentang Kami</li>
-        <li>Program Kami</li>
-        <li>Galeri</li>
-        <li>Produk</li>
-        <li>Artikel</li>
-        <li>E-tiket</li>
-        <button className="btn-contact mobile-btn">Hubungi Kami</button>
-      </ul>
+  <li>Beranda</li>
+  <li>Tentang Kami</li>
+
+  {/* DROPDOWN PROGRAM */}
+  <li className="dropdown">
+    Program Kami 
+    <ul className="dropdown-menu">
+      <li>Hydroponic</li>
+      <li>Venue Workshop</li>
+      <li>Peternakan</li>
+      <li>Venue Alam</li>
+    </ul>
+  </li>
+
+  <li>Galeri</li>
+
+  {/* DROPDOWN PRODUK */}
+  <li className="dropdown">
+    Produk
+    <ul className="dropdown-menu">
+      <li>Semua Produk</li>
+      <li>Peternakan</li>
+      <li>Sayuran</li>
+      <li>Saprodi</li>
+    </ul>
+  </li>
+
+  <li>Artikel</li>
+  <li>E-tiket</li>
+
+  <button className="btn-contact mobile-btn">Hubungi Kami</button>
+</ul>
 
       <button className="btn-contact desktop-btn">Hubungi Kami</button>
 
